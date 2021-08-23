f6d650c1f2d0914f64fb05091e06e6e519d48506
Dreamr Labs <36281831+dreamrsunite@users.noreply.github.com>
2021-08-23 08:04:00 -0700

    Update DreamrPlatformToken.sol

--- DreamrPlatformToken.sol
+++ DreamrPlatformToken.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity 0.8.0;
+pragma solidity 0.5.0;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/ownership/Ownable.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20Detailed.sol";
